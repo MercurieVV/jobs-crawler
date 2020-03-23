1857cf3b62862e54a8d82cd598991a78bfa5008a
MercurieVV <mercurievv@gmail.com>
2020-03-23 19:44:07 +0200

    Cleanup

--- src/main/scala/com/github/mercurievv/jobsearch/Module.scala
+++ src/main/scala/com/github/mercurievv/jobsearch/Module.scala
@@ -15,15 +15,6 @@ import org.http4s.client.Client
  * Contacts: email: mercurievvss@gmail.com Skype: 'grobokopytoff' or 'mercurievv'
  */
 class Module[F[_]: Monad, S[_]](client: Client[F])(implicit ce: ConcurrentEffect[F]) {
-  import org.http4s.client.blaze._
-  import zio._
-  import zio.interop.catz._
-
-  import scala.concurrent.ExecutionContext.global
-  //    implicit val cs: ContextShift[AIO] = cats.effect.IO.contextShift(global)
-  //    implicit val timer: Timer[AIO] = cats.effect.IO.timer(global)
-  //    val runtime = Runtime.default
-  //    import runtime._
   val dbStorage = new DynamodbJobsStorage()
   val jobs = new GetJobsFromStackowerflow[F](new StackowerflowJobsApi[F](client))
   val jobsOps : JobsStorage[F, S] = ???
