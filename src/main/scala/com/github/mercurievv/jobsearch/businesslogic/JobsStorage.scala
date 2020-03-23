ac5805eb9ad366546cf2b4cbd4fb3fccd29c822a
MercurieVV <mercurievv@gmail.com>
2020-03-23 19:38:41 +0200

    Jobs storage trait

---
+++ src/main/scala/com/github/mercurievv/jobsearch/businesslogic/JobsStorage.scala
@@ -0,0 +1,16 @@
+package com.github.mercurievv.jobsearch.businesslogic
+
+import com.github.mercurievv.jobsearch.model.Job
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Victor Mercurievv
+ * Date: 3/19/2020
+ * Time: 9:48 PM
+ * Contacts: email: mercurievvss@gmail.com Skype: 'grobokopytoff' or 'mercurievv'
+ */
+trait JobsStorage[F[_], S[_]] {
+  def filterNewJobsOnly(allJobs: S[Job]): S[Job]
+  def saveJobsToDb(jobs: S[Job]): F[Unit]
+
+}
