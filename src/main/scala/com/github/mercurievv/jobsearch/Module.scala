2b115a562644b4c9b9ea77af3e52771fdf64ab3c
Victor Kalinin <mercurievv@gmail.com>
2020-05-31 22:19:10 +0300

    Implement JobsServer for StackOwerflow.com

--- src/main/scala/com/github/mercurievv/jobsearch/Module.scala
+++ src/main/scala/com/github/mercurievv/jobsearch/Module.scala
@@ -1,10 +1,10 @@
 package com.github.mercurievv.jobsearch
 
-import cats.Monad
+import cats.{Monad, ~>}
 import cats.effect.ConcurrentEffect
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClientBuilder
-import com.github.mercurievv.jobsearch.businesslogic.{CollectJobs, JobsStorage}
+import com.github.mercurievv.jobsearch.businesslogic.{CollectJobs, JobsServer, JobsStorage}
 import com.github.mercurievv.jobsearch.persistence.DynamodbJobsStorage
 import com.github.mercurievv.jobsearch.stackowerflow.{GetJobsFromStackowerflow, StackowerflowJobsApi}
 import org.http4s.client.Client
@@ -16,7 +16,7 @@ import org.http4s.client.Client
  * Time: 4:14 PM
  * Contacts: email: mercurievvss@gmail.com Skype: 'grobokopytoff' or 'mercurievv'
  */
-class Module[F[_]: Monad, S[_]](httpClient: Client[F])(implicit ce: ConcurrentEffect[F]) {
+class Module[F[_]: Monad, S[_]: Monad](httpClient: Client[F], seqToS: Seq ~> S)(implicit ce: ConcurrentEffect[F]) {
   private val dbclient = {
     val conf = new EndpointConfiguration("http://localhost:8000", "us-east-1")
     AmazonDynamoDBAsyncClientBuilder.standard().withEndpointConfiguration(conf).build()
@@ -25,4 +25,5 @@ class Module[F[_]: Monad, S[_]](httpClient: Client[F])(implicit ce: ConcurrentEf
   val jobs = new GetJobsFromStackowerflow[F](new StackowerflowJobsApi[F](httpClient))
   val jobsOps : JobsStorage[F, S] = ???
   val collectJobs = new CollectJobs(jobsOps)
+  val jobsServers = List(new JobsServer.StackOwerflow(new GetJobsFromStackowerflow[F](new StackowerflowJobsApi[F](httpClient)), seqToS))
 }
--- src/main/scala/com/github/mercurievv/jobsearch/businesslogic/JobsServer.scala
+++ src/main/scala/com/github/mercurievv/jobsearch/businesslogic/JobsServer.scala
@@ -1,6 +1,12 @@
 package com.github.mercurievv.jobsearch.businesslogic
 
+import cats.data.NonEmptyChain
+import cats._
+import cats.implicits._
+import com.github.mercurievv.jobsearch.Errorable
 import com.github.mercurievv.jobsearch.model.Job
+import com.github.mercurievv.jobsearch.stackowerflow.GetJobsFromStackowerflow
+import simulacrum._
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,7 +15,18 @@ import com.github.mercurievv.jobsearch.model.Job
  * Time: 10:39 PM
  * Contacts: email: mercurievvss@gmail.com Skype: 'grobokopytoff' or 'mercurievv'
  */
-trait JobsServer[F[_], S[_]] {
-  def getJobsFromServer: F[S[Job]]
+import enumeratum._
+
+sealed trait JobsServer[F[_], S[_]] /*extends EnumEntry*/ {
+  def getJobsFromServer: F[S[Errorable[Job]]]
+
+}
+
+object JobsServer /*extends Enum[JobsServer[*, *]] */{
+//  val values = findValues
+
+  class StackOwerflow[F[_] : Monad, S[_]](getJobs: GetJobsFromStackowerflow[F], seqToS: Seq ~> S) extends JobsServer[F, S] {
+    override def getJobsFromServer: F[S[Errorable[Job]]] = getJobs.getJobs.map(seqToS(_))
+  }
 
 }
